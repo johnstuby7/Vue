b742dc12efc18b93b8df88e72adbbb21b5a3d98f
John Stuby <johnstuby7@gmail.com>
2023-05-01 15:48:03 -0600

    Created a computed value for custom styling

--- project2/app.js
+++ project2/app.js
@@ -2,12 +2,23 @@ let vm = Vue.createApp({
   data() {
     return {
       isPurple: false,
-      selectedColor: ''
+      selectedColor: '',
+      size: 150,
     }
   },
   computed: {
     circle_classes() {
       return { purple: this.isPurple}
-    }
+    },
+    circle_styles() {
+      return [
+        {
+          width: this.size + "px",
+          height: this.size + "px",
+          lineHeight: this.size + "px",
+        },
+        { transform: "rotate(-30deg)" },
+      ];
+    },
   }
 }).mount('#app')
