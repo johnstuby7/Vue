1b0785b73f617501400868217671a1395099e1e3
John Stuby <johnstuby7@gmail.com>
2023-05-03 15:47:19 -0600

    Basic exmaple of importing/exporting functionality from one script to another

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-const a = 4;
+const script2 = require('./script2.js')
+
+const a = script2.largeNumber;
 const b = 5;
 
-setTimeout(() => {
-  console.log(a + b);
-}, 3000)
+// setTimeout(() => {
+//   console.log(a + b);
+// }, 3000)
 
+console.log(a + b);
 console.log(__dirname);
 
