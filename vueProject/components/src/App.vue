fabe0245fa825dbfe45c5e0debb6db96d848911b
John Stuby <johnstuby7@gmail.com>
2023-05-05 11:04:49 -0600

    emiting events for props, notes updated with examples

--- vueProject/components/src/App.vue
+++ vueProject/components/src/App.vue
@@ -1,14 +1,13 @@
 <template>
   <h3>Hey</h3>
-
-  <button type="button" @click="age++">Update Age</button>
   <Greeting :age="age" />
-  <User :age="age" />
+  <User :age="age" @age-change="updateAge" />
 </template>
 
 <script>
 import Greeting from "@/components/Greeting.vue"
 import User from "@/components/User.vue"
+
 export default {
   name: 'App',
   components: { 
@@ -18,6 +17,11 @@ export default {
   data() {
     return {
       age: 20
+    }
+  },
+  methods: {
+    updateAge(num) {
+      this.age += num
     }
   }
 }
