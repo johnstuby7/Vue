2af844a971f2e4817f2487188a1de54f4edf5335
John Stuby <johnstuby7@gmail.com>
2023-05-05 15:11:24 -0600

    Added in animation library to be able to use created animations

--- animations/src/App.vue
+++ animations/src/App.vue
@@ -27,7 +27,10 @@
 
   <ul>
     <!-- transition group is for animating items in a loop -->
-    <transition-group name="fade">
+    <transition-group name="fade" 
+      enter-active-class="animate__animated animate__flipInX"
+      leave-active-class="animate__animated animate__flipOutX"
+    >
       <li v-for="(number, index) in numbers" :key="number"
           @click="removeItem(index)">
         {{  number  }}
@@ -92,6 +95,14 @@ export default {
 };
 </script>
 <style>
+.animate__flipOutX {
+  position: absolute;
+}
+
+.animate__animated {
+  animation-duration: 1.5s;
+}
+
 li {
   font-size: 22px;
   cursor: pointer;
