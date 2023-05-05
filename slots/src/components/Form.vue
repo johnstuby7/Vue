2b05388ee22518ef27872d8c0f233507e44c74fd
John Stuby <johnstuby7@gmail.com>
2023-05-05 11:32:58 -0600

    Created slot example, set up basic form

---
+++ slots/src/components/Form.vue
@@ -0,0 +1,15 @@
+<template>
+  <form>
+    <div class="help">
+      <p>This is some hlep text.</p>
+    </div>
+    <div class="fields">
+      <input type="text" placeholder="email">
+      <input type="text" placeholder="username">
+      <input type="password" placeholder="password">
+    </div>
+    <div class="buttons">
+      <button type="submit">Submit</button>
+    </div>
+  </form>
+</template>
