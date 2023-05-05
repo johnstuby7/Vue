6a19ff6c6af58ea06ee72d25c9c66743c6e583bc
John Stuby <johnstuby7@gmail.com>
2023-05-05 10:40:42 -0600

    Basic prop example, Age is defined in App.vue and is able to be passed a prop to user and is accessible for display in the user view

--- vueProject/components/src/App.vue
+++ vueProject/components/src/App.vue
@@ -1,14 +1,22 @@
 <template>
-  <p>Hey</p>
+  <h3>Hey</h3>
   <Greeting />
+  <User :age="age" />
 </template>
 
 <script>
 import Greeting from "@/components/Greeting.vue"
+import User from "@/components/User.vue"
 export default {
   name: 'App',
   components: { 
-    Greeting 
+    Greeting,
+    User
   },
+  data() {
+    return {
+      age: 20
+    }
+  }
 }
 </script>
---
+++ vueProject/components/src/components/User.vue
@@ -0,0 +1,9 @@
+<template>
+  <p>The user is {{ age }} years old</p>
+</template>
+<script>
+export default {
+  name: "User",
+  props: ["age"]
+}
+</script>
