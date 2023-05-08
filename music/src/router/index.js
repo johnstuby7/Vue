6936ff5bf2a6b3a5098b26ef5805b9e7705b8b8e
John Stuby <johnstuby7@gmail.com>
2023-05-08 16:13:50 -0600

    Updated Notes, added comments for alias example

--- music/src/router/index.js
+++ music/src/router/index.js
@@ -1,14 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
-// import Home from "../views/Home.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    // {
-    //   path: "/",
-    //   name: "home",
-    //   component: Home,
-    // },
     {
       path: '/about',
       name: 'about',
