f01a24b987cd1caa8e28e0ca7d7313b82535c9e6
John Stuby <johnstuby7@gmail.com>
2023-05-08 19:53:07 -0600

    Added in a slot-prop example

--- music/src/components/Auth.vue
+++ music/src/components/Auth.vue
@@ -60,7 +60,7 @@
             </button>
           </form>
           <!-- Registration Form -->
-          <vee-form v-show="tab === 'register'" :validation-schema="schema">
+          <vee-form v-show="tab === 'register'" :validation-schema="schema" @submit="register">
             <!-- Name -->
             <div class="mb-3">
               <label class="inline-block mb-2">Name</label>
@@ -156,5 +156,10 @@ export default {
       modalVisibility: "isOpen",
     }),
   },
+  methods: {
+    register(values) {
+      console.log(values);
+    }
+  }
 };
 </script>
