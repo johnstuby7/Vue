2d783e2dae255c0be7ea915624d27dcd5e2ec3c3
John Stuby <johnstuby7@gmail.com>
2023-05-09 11:08:41 -0600

    Refactore register form logic for auth into the firebase file

--- music/src/includes/firebase.js
+++ music/src/includes/firebase.js
@@ -1,8 +1,8 @@
 import firebase from 'firebase/app'
 import 'firebase/auth'
+import 'firebase/firestore'
 
 // Your web app's Firebase configuration
-
 const firebaseConfig = {
   apiKey: 'AIzaSyDNIfHTQY9LNlx5y71AoLhs5MUuLwJeF-8',
   authDomain: 'music-3ea4c.firebaseapp.com',
@@ -11,4 +11,9 @@ const firebaseConfig = {
   appId: '1:990451646499:web:ef48394875108fc90e7196'
 }
 
-export default firebase.initializeApp(firebaseConfig)
+firebase.initializeApp(firebaseConfig)
+
+const auth = firebase.auth()
+const db = firebase.firestore()
+
+export { auth, db }
