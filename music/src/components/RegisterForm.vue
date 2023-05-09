6dfbe717ce12e8aaf9ef6bad960e1e0510f14261
John Stuby <johnstuby7@gmail.com>
2023-05-09 11:47:58 -0600

    Added in logic to show that a user has been logged in

--- music/src/components/RegisterForm.vue
+++ music/src/components/RegisterForm.vue
@@ -80,6 +80,9 @@
 
 <script>
 import { auth, usersCollection } from '../includes/firebase'
+import { mapWritableState } from 'pinia'
+import useUserStore from '@/stores/user'
+
 export default {
   name: 'RegisterForm',
   data() {
@@ -103,6 +106,9 @@ export default {
       reg_alert_variant: 'bg-blue-500',
       reg_alert_msg: 'Please wait! Your account is being created.'
     }
+  },
+  computed: {
+    ...mapWritableState(useUserStore, ['userLoggedIn'])
   },
   methods: {
     async register(values) {
@@ -134,6 +140,8 @@ export default {
         this.reg_alert_msg = 'An unexpected error occured. Please try again later.'
         return
       }
+
+      this.userLoggedIn = true
 
       this.reg_alert_variant = 'bg-green-500'
       this.reg_alert_msg = 'Success! Your account has been created.'
