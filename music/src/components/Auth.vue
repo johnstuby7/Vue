8287554126750e987d72224029aed159ebda1ee3
John Stuby <johnstuby7@gmail.com>
2023-05-08 19:00:54 -0600

    Added in email validation, updated email field to use validation

--- music/src/components/Auth.vue
+++ music/src/components/Auth.vue
@@ -72,9 +72,10 @@
             <!-- Email -->
             <div class="mb-3">
               <label class="inline-block mb-2">Email</label>
-              <input type="email"
+              <vee-field type="email" name="email"
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
                 placeholder="Enter Email" />
+              <ErrorMessage class="text-red-600" name="email" />
             </div>
             <!-- Age -->
             <div class="mb-3">
@@ -133,7 +134,7 @@ export default {
       tab: "login",
       schema: {
         name: "required|min:3|max:100|alpha_spaces",
-        emaiL: "required",
+        email: "required|min:3|max:100|email",
         age: "",
         password: "",
         confirm_password: "",
