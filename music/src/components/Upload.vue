66aa03e9e9494086cfcf03c2fc926230696fff0e
John Stuby <johnstuby7@gmail.com>
2023-05-10 17:53:08 -0600

    Created a firebase colelction for songs

--- music/src/components/Upload.vue
+++ music/src/components/Upload.vue
@@ -29,7 +29,7 @@
   </div>
 </template>
 <script>
-import { storage } from '@/includes/firebase'
+import { storage, auth, songsCollection } from '@/includes/firebase'
 
 export default {
   name: 'Upload',
@@ -79,7 +79,19 @@ export default {
 
             console.log(error)
           },
-          () => {
+          async () => {
+            const song = {
+              uid: auth.currentUser.uid,
+              display_name: auth.currentUser.display_name,
+              original_name: task.snapshot.ref.name,
+              modified_name: task.snapshot.ref.name,
+              genre: '',
+              comment_count: 0
+            }
+
+            song.url = await task.snapshot.ref.getDownloadUrl()
+            await songsCollection.add(song)
+
             this.uploads[uploadIndex].variant = 'bg-green-400'
             this.uploads[uploadIndex].icon = 'fas fa-check'
             this.uploads[uploadIndex].text_class = 'text-green-400'
