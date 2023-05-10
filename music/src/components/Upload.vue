3ad8371739918fc40c133417e84584d13d477e42
John Stuby <johnstuby7@gmail.com>
2023-05-10 11:52:59 -0600

    Updated notes to include rules for firebase, created progress bar method that will display current percentage complete

--- music/src/components/Upload.vue
+++ music/src/components/Upload.vue
@@ -11,45 +11,62 @@
         :class="{ 'bg-green-400 border-green-400 border-solid': is_dragover }" @drag.prevent.stop=""
         @dragstart.prevent.stop="" @dragend.prevent.stop="is_dragover = false" @dragover.prevent.stop="is_dragover = true"
         @dragenter.prevent.stop="is_dragover = true" @dragleave.prevent.stop="is_dragover = false"
-        @drop.prevent.stop="upload">
+        @drop.prevent.stop="upload($event)">
         <h5>Drop your files here</h5>
       </div>
       <hr class="my-6" />
       <!-- Progess Bars -->
-      <div class="mb-4">
+      <div class="mb-4" v-for="upload in uploads" :key="upload.name">
         <!-- File Name -->
-        <div class="font-bold text-sm">Just another song.mp3</div>
+        <div class="font-bold text-sm">{{ upload.name }}</div>
         <div class="flex h-4 overflow-hidden bg-gray-200 rounded">
           <!-- Inner Progress Bar -->
-          <div class="transition-all progress-bar bg-blue-400" style="width: 75%"></div>
-        </div>
-      </div>
-      <div class="mb-4">
-        <div class="font-bold text-sm">Just another song.mp3</div>
-        <div class="flex h-4 overflow-hidden bg-gray-200 rounded">
-          <div class="transition-all progress-bar bg-blue-400" style="width: 35%"></div>
-        </div>
-      </div>
-      <div class="mb-4">
-        <div class="font-bold text-sm">Just another song.mp3</div>
-        <div class="flex h-4 overflow-hidden bg-gray-200 rounded">
-          <div class="transition-all progress-bar bg-blue-400" style="width: 55%"></div>
+          <div class="transition-all progress-bar bg-blue-400" :class="bg - blue - 400"
+            :style="{ width: upload.current_progress + '%' }"></div>
         </div>
       </div>
     </div>
   </div>
 </template>
 <script>
+import { storage } from '@/includes/firebase'
+
 export default {
   name: 'Upload',
   data() {
     return {
-      is_dragover: false
+      is_dragover: false,
+      uploads: []
     }
   },
   methods: {
-    upload() {
+    upload($event) {
       this.is_dragover = false
+
+      const { files } = [...$event.dataTransfer.files]
+
+      files.forEach((file) => {
+        if (file.type !== 'audio/mpeg') {
+          return
+        }
+
+        // music-3ea4c.appspot.com
+        const storageRef = storage.ref()
+        const songsRef = storageRef.child(`songs/${file.name}`)
+        const task = songsRef.put(file)
+
+        this.uploads.push({
+          task,
+          current_progress: 0,
+          name: file.name
+        })
+
+        // This is will give us the percent of progress complete
+        task.on('state_changed', (snapshot) => {
+          const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+        })
+      })
+      console.log(files)
     }
   }
 }
