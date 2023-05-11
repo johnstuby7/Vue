704da7620a8a17bcdb30bf98bcc56eccd6e48c24
John Stuby <johnstuby7@gmail.com>
2023-05-11 15:13:31 -0600

    Added in functionality to create a infinite scrolling page, set the limit of songs to 25 per page

--- music/src/views/Home.vue
+++ music/src/views/Home.vue
@@ -44,7 +44,9 @@ export default {
   },
   data() {
     return {
-      songs: []
+      songs: [],
+      maxPerPage: 3,
+      pendingRequest: false
     }
   },
   async created() {
@@ -62,12 +64,28 @@ export default {
       const bottomOfWindow = Math.round(scrollTop) + innerHeight === offsetHeight
 
       if (bottomOfWindow) {
-        console.log('Bottom of window')
+        this.getSongs()
       }
     },
 
     async getSongs() {
-      const snapshots = await songsCollection.get()
+      if (this.pendingRequest) {
+        return
+      }
+
+      this.pendingRequest = true
+
+      let snapshots
+      if (this.songs.length) {
+        const lastDoc = await songsCollection.doc(this.songs[this.songs.length - 1].docID).get()
+        snapshots = await songsCollection
+          .orderBy('modified_name')
+          .startAfter(lastDoc)
+          .limit(this.maxPerPage)
+          .get()
+      } else {
+        snapshots = await songsCollection.orderBy('modified_name').limit(this.maxPerPage).get()
+      }
 
       snapshots.forEach((document) => {
         this.songs.push({
@@ -75,6 +93,8 @@ export default {
           ...document.data()
         })
       })
+
+      this.pendingRequest = false
     }
   }
 }
