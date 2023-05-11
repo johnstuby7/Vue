52a78c28f605f2143b4be594293e251f967ea1d7
John Stuby <johnstuby7@gmail.com>
2023-05-11 11:48:21 -0600

    Added in validation for the form and added error messaging for the form

--- music/src/components/CompositionItem.vue
+++ music/src/components/CompositionItem.vue
@@ -11,22 +11,25 @@
       </button>
     </div>
     <div v-show="showForm">
-      <form>
+      <!-- initial-values will load the input fields with data -->
+      <vee-form :validation-schema="schema" :initial-values="song" @submit="edit">
         <div class="mb-3">
           <label class="inline-block mb-2">Song Title</label>
-          <input type="text"
+          <vee-field type="text" name="modified_name"
             class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
             placeholder="Enter Song Title" />
+          <ErrorMessage class="text-red-600" name="modified_name" />
         </div>
         <div class="mb-3">
           <label class="inline-block mb-2">Genre</label>
-          <input type="text"
+          <vee-field type="text" name="genre"
             class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
             placeholder="Enter Genre" />
+          <ErrorMessage class="text-red-600" name="genre" />
         </div>
         <button type="submit" class="py-1.5 px-3 rounded text-white bg-green-600">Submit</button>
         <button type="button" class="py-1.5 px-3 rounded text-white bg-gray-600">Go Back</button>
-      </form>
+      </vee-form>
     </div>
   </div>
 </template>
@@ -35,13 +38,22 @@ export default {
   name: 'CompositionItem',
   data() {
     return {
-      showForm: false
+      showForm: false,
+      schema: {
+        modified_name: 'required',
+        genre: 'alpha_spaces'
+      }
     }
   },
   props: {
     song: {
       type: Object,
       required: true
+    }
+  },
+  methods: {
+    edit() {
+      console.log('You did it')
     }
   }
 }
