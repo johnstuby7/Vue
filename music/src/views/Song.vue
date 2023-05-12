c56123005e7f05350afeadace57a77bd9f9aead0
John Stuby <johnstuby7@gmail.com>
2023-05-12 11:42:33 -0600

    Added in comments form, now successfully stores comments in firebase

--- music/src/views/Song.vue
+++ music/src/views/Song.vue
@@ -24,14 +24,18 @@
         <i class="fa fa-comments float-right text-green-400 text-2xl"></i>
       </div>
       <div class="p-6">
+        <div class="text-white text-center font-bold p-4 mb04" v-if="comment_show_alert" :class="comment_alert_variant">
+          {{ comment_alert_message }}
+        </div>
         <!-- validation-schema allows for the  addition of a ruleset for the fields, set in the schema object-->
-        <vee-form :validation-schema="schema">
+        <vee-form :validation-schema="schema" @submit="addComment" v-if="userLoggedIn">
           <!-- adding the as to the vee-field allows you to treat it like whatever field type you want -->
           <vee-field as="textarea" name="comment"
             class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded mb-4"
             placeholder="Your comment here..."></vee-field>
           <ErrorMessage class="text-red-600" name="comment" />
-          <button type="submit" class="py-1.5 px-3 rounded text-white bg-green-600 block">
+          <button type="submit" class="py-1.5 px-3 rounded text-white bg-green-600 block"
+            :disabled="comment_in_submission">
             Submit
           </button>
         </vee-form>
@@ -121,7 +125,9 @@
   </ul>
 </template>
 <script>
-import { songsCollection } from '../includes/firebase'
+import { songsCollection, auth, commentsCollection } from '../includes/firebase'
+import { mapState } from 'pinia'
+import useUserStore from '@/stores/user'
 
 export default {
   name: 'Song',
@@ -130,8 +136,15 @@ export default {
       song: {},
       schema: {
         comment: 'required|min:3'
-      }
+      },
+      comment_in_submission: false,
+      comment_show_alert: false,
+      comment_alert_variant: 'bg-blue-500',
+      comment_alert_message: 'Please wait! your comment is being submitted'
     }
+  },
+  computed: {
+    ...mapState(useUserStore, ['userLoggedIn'])
   },
   async created() {
     // returns a document snapshot
@@ -142,6 +155,32 @@ export default {
       return
     }
     this.song = docSnapshot.data()
+  },
+  methods: {
+    // context can be used to reset the form, second option has built in methods we can use
+    async addComment(values, { resetForm }) {
+      this.comment_in_submission = true
+      this.comment_show_alert = true
+      this.comment_alert_variant = 'bg-blue-500'
+      this.comment_alert_message = 'Please wait! your comment is being submitted'
+
+      // remember to add a comment collection to firebase
+      const comment = {
+        content: values.comment,
+        datePosted: new Date().toString(),
+        sid: this.$route.params.id,
+        name: auth.currentUser.displayName,
+        uid: auth.currentUser.uid
+      }
+
+      await commentsCollection.add(comment)
+
+      this.comment_in_submission = false
+      this.comment_alert_variant = 'bg-green-500'
+      this.comment_alert_message = 'COmment Added!'
+
+      resetForm()
+    }
   }
 }
 </script>
