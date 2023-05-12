6fc0d2f05502f9ddd8ea243da4a7e948fc8bb9c7
John Stuby <johnstuby7@gmail.com>
2023-05-12 12:31:22 -0600

    Updated notes, added in comment count

--- music/src/views/Song.vue
+++ music/src/views/Song.vue
@@ -20,7 +20,7 @@
     <div class="bg-white rounded border border-gray-200 relative flex flex-col">
       <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200">
         <!-- Comment Count -->
-        <span class="card-title">Comments (15)</span>
+        <span class="card-title">Comments ({{ song.comment_count }})</span>
         <i class="fa fa-comments float-right text-green-400 text-2xl"></i>
       </div>
       <div class="p-6">
@@ -130,6 +130,12 @@ export default {
       }
 
       await commentsCollection.add(comment)
+
+      // During submission of a comment, the comment count will be increased
+      this.song.comment_count += 1
+      await songsCollection.doc(this.$route.params.id).update({
+        comment_count: this.song.comment_count
+      })
 
       this.getComments()
 
