42a423169651adc6b6905a7e0eb8fd01a507dc95
John Stuby <johnstuby7@gmail.com>
2023-05-13 11:25:26 -0600

    Added in a couple modifiers to the icon directive

--- music/src/directives/icon.js
+++ music/src/directives/icon.js
@@ -1,5 +1,22 @@
 export default {
-  beforeMount(el) {
-    el.innerHTML += '<i class="fa fa-headphones-alt float-right text-green-400 text-xl"></i>'
+  beforeMount(el, binding) {
+    // the binding.value allows this directive to be reused for any icon
+    let iconClass = `fa fa-${binding.value} text-2xl`
+
+    if (binding.arg === 'full') {
+      iconClass = binding.value
+    }
+
+    if (binding.modifiers.right) {
+      iconClass += ' float-right'
+    }
+
+    if (binding.modifiers.yellow) {
+      iconClass += ' text-yellow-400'
+    } else {
+      iconClass = +' text-green-400'
+    }
+
+    el.innerHTML += `<i class="${iconClass}"></i>`
   }
 }
