392173e136f412d236b9a1e38ecafb0ee5562357
John Stuby <johnstuby7@gmail.com>
2023-05-14 07:54:56 -0600

    Added a french translation file, added in the first translation tot he home page

--- music/src/includes/i18n.js
+++ music/src/includes/i18n.js
@@ -1,10 +1,12 @@
 import { createI18n } from 'vue-i18n'
 import en from '@/locales/en.json'
+import fr from '@/locales/fr.json'
 
 export default createI18n({
-  locale: 'en',
+  locale: 'fr',
   fallbackLocale: 'en',
   messages: {
-    en
+    en,
+    fr
   }
 })
