4846a708158feb3e69f31fe89188e093c24fa45a
John Stuby <johnstuby7@gmail.com>
2023-05-14 19:36:39 -0600

    Added in logic to help with the loading of the webpage on slow computers, instead of doing piece by piec it will now show the whole website at once

--- music/src/views/Song.vue
+++ music/src/views/Song.vue
@@ -101,21 +101,23 @@ export default {
       })
     }
   },
-  async created() {
+  async beforeRouteEnter(to, from, next) {
     // returns a document snapshot
-    const docSnapshot = await songsCollection.doc(this.$route.params.id).get()
+    const docSnapshot = await songsCollection.doc(to.params.id).get()
 
-    if (!docSnapshot.exists) {
-      this.$router.push({ name: 'home' })
-      return
-    }
+    next((vm) => {
+      if (!docSnapshot.exists) {
+        vm.$router.push({ name: 'home' })
+        return
+      }
 
-    const { sort } = this.$route.query
+      const { sort } = vm.$route.query
 
-    this.sort = sort === '1' || sort === '2' ? sort : '1'
+      vm.sort = sort === '1' || sort === '2' ? sort : '1'
 
-    this.song = docSnapshot.data()
-    this.getComments()
+      vm.song = docSnapshot.data()
+      vm.getComments()
+    })
   },
   methods: {
     ...mapActions(usePlayerStore, ['newSong']),
