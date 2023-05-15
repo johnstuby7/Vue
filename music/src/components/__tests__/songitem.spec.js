76b2703943a94021424d06e95fbd96f56ee86147
John Stuby <johnstuby7@gmail.com>
2023-05-15 09:14:22 -0600

    Fixed minor issue with home spec spelling, updated tests for songitem

--- music/src/components/__tests__/songitem.spec.js
+++ music/src/components/__tests__/songitem.spec.js
@@ -23,4 +23,24 @@ describe('SongItem.vue', () => {
 
     expect(compositionAuthor.text()).toBe(song.display_name)
   })
+
+  test('render song.docID in id attribute', () => {
+    const song = {
+      docID: 'abc'
+    }
+
+    const wrapper = shallowMount(SongItem, {
+      props: {
+        song
+      },
+      global: {
+        components: {
+          'router-link': RouterLinkStub
+        }
+      }
+    })
+
+    expect(wrapper.classes()).toContain(`song-id-${song.docID}`)
+    // expect(wrapper.attributes().id).toBe(`song-id-${song.docID}`)
+  })
 })
