0baebaf5d6432c9bbd6ff9b72f8d6e6f0ff4b19d
John Stuby <johnstuby7@gmail.com>
2023-05-15 08:41:31 -0600

    Fixed tests to pass correctly

--- music/src/components/__tests__/about.spec.js
+++ music/src/components/__tests__/about.spec.js
@@ -1,8 +1,13 @@
 import About from '@/views/About.vue'
 import { mount } from '@vue/test-utils'
+import { expect, test, describe } from 'vitest'
 
 describe('About.vue', () => {
   test('renders inner text', () => {
-    const wrapper = mount(About)
+    const wrapper = mount(About, {
+      shallow: true
+    })
+
+    expect(wrapper.text()).toContain('about')
   })
 })
--- music/src/components/__tests__/example.spec.js
+++ music/src/components/__tests__/example.spec.js
@@ -1,3 +1,4 @@
+import { expect, test } from 'vitest'
 test('sanity test', () => {
   expect(true).toBe(true)
 })
