4301b3bcf26dd5f31fcbf974a2b1e3b1348c8a7d
John Stuby <johnstuby7@gmail.com>
2023-05-15 15:56:47 -0600

    created a router spec test

---
+++ music/src/components/__tests__/router.spec.js
@@ -0,0 +1,25 @@
+import { shallowMount, RouterLinkStub } from '@/vue/test-utils'
+import SongItem from '@/components/SongItem.vue'
+import { expect, test, describe } from 'vitest'
+
+describe('router', () => {
+  test('renders router link', () => {
+    const song = {
+      docID: 'abc'
+    }
+
+    const wrapper = shallowMount(SongItem, {
+      propsData: { song },
+      global: {
+        components: {
+          'router-link': RouterLinkStub
+        }
+      }
+    })
+
+    expect(wrapper.findComponent(RouterLinkStub).props().to).toEqual({
+      name: 'song',
+      params: { id: song.docID }
+    })
+  })
+})
