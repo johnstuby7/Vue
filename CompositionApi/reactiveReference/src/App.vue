8b429aa69cacc44bfea8d14eb87c4652ef4b3da2
John Stuby <johnstuby7@gmail.com>
2023-05-16 11:32:23 -0600

    Moved mixin example to its own folder, added in a reactiveReference example, used it to increment a value on button click{

---
+++ CompositionApi/reactiveReference/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <p>{{ num }}</p>
+    <button type="button" @click.prevent="increment">Click Me</button>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue"
+
+export default {
+  name: "App",
+  setup() {
+    let num = ref(0);
+
+    function increment() {
+      num.value++;
+    }
+
+    return {
+      num,
+      increment
+    }
+  }
+};
+</script>
