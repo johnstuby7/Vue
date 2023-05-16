9adb2da8291eb07857235f2a2362906ed1455926
John Stuby <johnstuby7@gmail.com>
2023-05-16 15:41:29 -0600

    Added in example of seperation of concerns by adding in a validate-email file to handle validation for emails

--- ControlledComponents/src/components/EmailInput.vue
+++ ControlledComponents/src/components/EmailInput.vue
@@ -1,19 +1,22 @@
 <template>
   <input type="email" class="form-control" placeholder="E-mail" :value="email" @input="update($event)" :class="{
-    'is-valid':
-      email.length >= 4 && email.length > 0,
+    'is-valid': validateEmail(email)
+    ,
     'is-invalid':
-      email.length < 4 && email.length > 0,
+      !validateEmail()
   }" />
 </template>
 <script>
+import validateEmail from '../validate-email'
+
 export default {
   name: "EmailInput",
   props: ['email'],
   methods: {
     update($event) {
       this.$emit('update:email', $event.target.value)
-    }
+    },
+    validateEmail()
   }
 }
 </script>
