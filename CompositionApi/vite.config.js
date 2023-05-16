bf801a831f4bb7f0e6a4a8195d2b86672207e5dd
John Stuby <johnstuby7@gmail.com>
2023-05-16 11:21:19 -0600

    Created new folder for compositionAPI section, created first example of a mixin file, fixed a few minor issues that arose

---
+++ CompositionApi/vite.config.js
@@ -0,0 +1,31 @@
+import { fileURLToPath, URL } from "node:url";
+
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
+import vue from "@vitejs/plugin-vue";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  test: {
+    global: true,
+  },
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: { enabled: true },
+      manifest: {
+        name: "Music App",
+        theme_color: "#ff5e3a",
+      },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,png,jpg}"],
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
